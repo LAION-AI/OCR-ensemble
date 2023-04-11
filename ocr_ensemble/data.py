24cde6924ae5bc18e819beec4fed70c3554e292d
Chris Wendler <chris.wendler@inf.ethz.ch>
2023-04-11 19:26:11 +0200

    trained linear probe on text/notext

--- ocr_ensemble/data.py
+++ ocr_ensemble/data.py
@@ -20,7 +20,27 @@ def load_dataset_1K():
             number_sample_per_shard=100,
             distributor="multiprocessing",
         )
-    dataset = wds.WebDataset(path + "/{00000..00010}.tar").decode("rgb").to_tuple("jpg", "txt")
+    dataset = wds.WebDataset(path + "/{00000..00010}.tar").decode("rgb").to_tuple("jpg", "txt", "__key__")
     return dataset
 
-        
+
+def load_dataset_10K():
+    path = "../data/laion2b-en-10K"
+    if not os.path.exists(path):
+        print("downloading dataset...")
+        download(
+            processes_count=6,
+            thread_count=12,
+            url_list="../data/laion2b-en-10K.parquet",
+            image_size=256,
+            output_folder=path,
+            output_format="webdataset",
+            input_format="parquet",
+            url_col="URL",
+            caption_col="TEXT",
+            enable_wandb=False,
+            number_sample_per_shard=1000,
+            distributor="multiprocessing",
+        )
+    dataset = wds.WebDataset(path + "/{00000..0010}.tar").decode("rgb").to_tuple("jpg", "txt", "__key__")
+    return dataset
