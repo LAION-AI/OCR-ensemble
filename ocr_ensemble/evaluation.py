9e522a7ffa4451b66e8b38977e73d7502e05158d
Chris Wendler <chris.wendler@inf.ethz.ch>
2023-05-30 12:59:43 +0200

    notebook for computing scores

--- ocr_ensemble/evaluation.py
+++ ocr_ensemble/evaluation.py
@@ -1,11 +1,14 @@
 from weighted_levenshtein import lev
 import numpy as np
 
-def average_levenshtein(gts, preds):
-    levs = []
-    for gt, pred in zip(gts, preds):
-        levs += [lev(gt.encode("ascii", "ignore").decode(), pred.encode("ascii", "ignore").decode())]
-    return np.mean(levs)
-
 def levenshtein(gt, pred):
     return lev(gt.encode("ascii", "ignore").decode(), pred.encode("ascii", "ignore").decode())
+
+def levenshtein_lower(gt, pred):
+    return lev(gt.lower().encode("ascii", "ignore").decode(), pred.lower().encode("ascii", "ignore").decode())
+
+def average_levenshtein(gts, preds, dist=levenshtein):
+    levs = []
+    for gt, pred in zip(gts, preds):
+        levs += [dist(gt, pred)]
+    return np.mean(levs)
